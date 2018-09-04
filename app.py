7aa42f1f0dbb7fafd02436adbac4745a6074f3b1
gunes <gunessenturk@gmail.com>
2018-09-03 21:13:37 -0400

    .. ... ..

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        db_code = "EOD"
+        ds_code = "HD"
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        url = 'https://www.quandl.com/api/v3/datasets/'+db_code+'/'+ds_code+'?column_index=4&start_date='+start_date+'&end_date='+end_date+'&api_key=D75Ftay9wEKd3jtrYJG8'
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+        return redirect('/graph')
+
+@app.route('/graph')
+def graph():
+    return render_template('stocks.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(port=33507, debug=True)
